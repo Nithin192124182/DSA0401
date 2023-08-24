dbfd20ad40cf295db556d191db0b875cb64a3617
Nithin192124182 <112066100+Nithin192124182@users.noreply.github.com>
2023-08-24 10:47:14 +0530

    Add files via upload

---
+++ no12.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+months=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+temp=[18,22,25,28,32,40,39,30,29,24,23,20]
+plt.plot(months,temp,marker='*',color='b',linestyle='-',linewidth=2.0)
+plt.show()
